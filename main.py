7e9408d4c18845e23e9acf271bad4d59f8aacf7a
Beta_Dext <87372653+PrajyotDaphal30@users.noreply.github.com>
2022-06-29 21:24:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+from speak import *
+from SpeechRecognition import *
+from SolarBodieName import *
+
+if __name__ == "__main__":
+    startup()
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+     # Logic for executing tasks based on query
+        if "number of planets" in query:
+            Solarbodiename('name')
